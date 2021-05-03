52760059aa35981a117e2b3985f8b3180de41c6f
mark <mark@localhost.localdomain>
2021-05-03 13:31:34 +0300

    kls

--- src/project/project/Player.java
+++ src/project/project/Player.java
@@ -1,11 +1,19 @@
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+public class Player implements MultimediaControl {
 
-public class Player implements MultimediaControl{
-    public static void main(String [] args) {
-        
+    public static void play() {
+        System.out.println("Playing from other player");
+
+    }
+    public static void previous() {
+        System.out.println("Previously on other player");
+    }
+    public static void stop() {
+        System.out.println("Stopping other player");
     }
 
-    public void play() {
-        
+    public static void main(String [] args) {
+        play();
+        previous();
+        stop();
     }
 }
